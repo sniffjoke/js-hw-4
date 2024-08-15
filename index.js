c9d604d3c36b43f545f2e698af307a9962313d38
sniffjoke <sniffjoke@gmail.com>
2024-08-15 21:10:17 +0200

    6-9 Map Set

--- index.js
+++ index.js
@@ -82,4 +82,37 @@
 
 // console.log(objectToArray(obj))
 
+// 6
+
+// const newMap = new Map();
+//
+// newMap.set('Bob', 4)
+// newMap.set('Jack', 5)
+// newMap.set('John', 6)
+// newMap.set('Mike', 7)
+// newMap.set('David', 8)
+//
+// console.log(newMap.get('Bob'))
+
+// let products = new Map([
+//     ['Milk', 1.2],
+//     ['Bread', 1.5],
+//     ['Cheese', 2.5],
+// ]);
+//
+// products.delete('Bread')
+// console.log(products.has('Bread'))
+// console.log(products)
+
+// let fruits = new Set(['apple', 'banana', 'mango']);
+
+// fruits.delete('banana')
+// console.log(fruits.has('banana'));
+// console.log(fruits)
+
+// const numbers = [1, 1, 4, 6, 7, 8, 9, 0, 0, 10, 10, 11]
+//
+// const result = new Set([...numbers])
+//
+// console.log(result)
 
